b69f2aaa956d0f15f7e7e138d7d387a5406205d2
Viviansol <viviansan93@gmail.com.br>
2022-11-10 15:07:34 -0300

    added response method

---
+++ src/response/response.go
@@ -0,0 +1,23 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+	if erro := json.NewEncoder(w).Encode(data); erro != nil {
+		log.Fatal(erro)
+	}
+}
+
+func Erro(w http.ResponseWriter, statusCode int, erro error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: erro.Error(),
+	})
+
+}
